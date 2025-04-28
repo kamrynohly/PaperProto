b0c9cbf5bcf82c1377e5b7c3f93025870be2d5c7
Grace Li <45218628+grxxce@users.noreply.github.com>
2025-04-27 21:57:53 -0400

    Delete .next directory

--- .next/static/chunks/app_create_page_32181b80.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_46746419._.js",
-  "static/chunks/_9776f787._.js"
-],
-    source: "dynamic"
-});
--- .next/static/chunks/app_games_[id]_page_32181b80.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_fadb717d._.js",
-  "static/chunks/node_modules_next_75e52325._.js"
-],
-    source: "dynamic"
-});
