f44a8393f3deff205c9c8de47050951142fd840a
Kamryn Ohly <kamohly@gmail.com>
2025-04-28 15:44:17 -0400

    commented out metadata

--- app/metadata.js
+++ app/metadata.js
@@ -1,27 +1,27 @@
-// app/metadata.js
-export const metadata = {
-    title: 'PaperProto - From pen to play',
-    description: 'Transform hand-drawn sketches into playable prototypes',
-    openGraph: {
-      title: 'PaperProto - From pen to play',
-      description: 'Transform hand-drawn sketches into playable prototypes',
-      url: 'https://paper-proto.com',
-      siteName: 'PaperProto',
-      images: [
-        {
-          url: '/og-image.png', // Place this image in your public folder
-          width: 1200,
-          height: 630,
-          alt: 'PaperProto - From pen & paper straight to play!',
-        },
-      ],
-      locale: 'en_US',
-      type: 'website',
-    },
-    twitter: {
-      card: 'summary_large_image',
-      title: 'PaperProto - From pen & paper straight to play!',
-      description: 'Transform hand-drawn sketches into playable prototypes',
-      images: ['/og-image.png'],
-    },
-}
+// // app/metadata.js
+// export const metadata = {
+//     title: 'PaperProto - From pen to play',
+//     description: 'Transform hand-drawn sketches into playable prototypes',
+//     openGraph: {
+//       title: 'PaperProto - From pen to play',
+//       description: 'Transform hand-drawn sketches into playable prototypes',
+//       url: 'https://paper-proto.com',
+//       siteName: 'PaperProto',
+//       images: [
+//         {
+//           url: '/og-image.png', // Place this image in your public folder
+//           width: 1200,
+//           height: 630,
+//           alt: 'PaperProto - From pen & paper straight to play!',
+//         },
+//       ],
+//       locale: 'en_US',
+//       type: 'website',
+//     },
+//     twitter: {
+//       card: 'summary_large_image',
+//       title: 'PaperProto - From pen & paper straight to play!',
+//       description: 'Transform hand-drawn sketches into playable prototypes',
+//       images: ['/og-image.png'],
+//     },
+// }
