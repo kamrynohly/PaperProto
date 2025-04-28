ae67c44b1822b00a6fbab192dbac9bd1edfe86e1
Grace Li <graceli.contact@gmail.com>
2025-04-28 16:00:41 -0400

    add metadata, please be kind and work

--- app/metadata.js
+++ app/metadata.js
@@ -1,27 +1,27 @@
-// // app/metadata.js
-// export const metadata = {
-//     title: 'PaperProto - From pen to play',
-//     description: 'Transform hand-drawn sketches into playable prototypes',
-//     openGraph: {
-//       title: 'PaperProto - From pen to play',
-//       description: 'Transform hand-drawn sketches into playable prototypes',
-//       url: 'https://paper-proto.com',
-//       siteName: 'PaperProto',
-//       images: [
-//         {
-//           url: '/og-image.png', // Place this image in your public folder
-//           width: 1200,
-//           height: 630,
-//           alt: 'PaperProto - From pen & paper straight to play!',
-//         },
-//       ],
-//       locale: 'en_US',
-//       type: 'website',
-//     },
-//     twitter: {
-//       card: 'summary_large_image',
-//       title: 'PaperProto - From pen & paper straight to play!',
-//       description: 'Transform hand-drawn sketches into playable prototypes',
-//       images: ['/og-image.png'],
-//     },
-// }
+export const metadata = {
+    title: 'PaperProto - From pen to play',
+    description: 'Transform hand-drawn sketches into playable prototypes',
+    metadataBase: new URL('https://paper-proto.com'),
+    openGraph: {
+      title: 'PaperProto - From pen to play',
+      description: 'Transform hand-drawn sketches into playable prototypes',
+      url: 'https://paper-proto.com',
+      siteName: 'PaperProto',
+      images: [
+        {
+          url: '/opengraph-image', // This path should be relative to metadataBase
+          width: 1200,
+          height: 630,
+          alt: 'PaperProto - From pen & paper straight to play!',
+        },
+      ],
+      locale: 'en_US',
+      type: 'website',
+    },
+    twitter: {
+      card: 'summary_large_image', // This was missing and is required
+      title: 'PaperProto - From pen & paper straight to play!',
+      description: 'Transform hand-drawn sketches into playable prototypes',
+      images: ['/opengraph-image'], // Make sure this path is correct
+    },
+}
--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,9 @@ const nextConfig = {
         'firebasestorage.googleapis.com',
       ],
     },
+    experimental: {
+        optimizeFonts: true,
+    }
   }
   
   export default nextConfig;
