b3ee3330104fe501d2e75cc34cc62e3b95c4b139
Grace Li <graceli.contact@gmail.com>
2025-05-03 23:41:40 -0400

    fix borders

--- client/app/layout.js
+++ client/app/layout.js
@@ -39,10 +39,10 @@ export default function RootLayout({ children }) {
       <body>
         <AuthProvider>
           {/* Decorative pixelated corners for that retro UI feel */}
-          <div className="absolute top-0 left-0 w-12 h-12 border-t-4 border-l-4 border-pink-500"></div>
-          <div className="absolute top-0 right-0 w-12 h-12 border-t-4 border-r-4 border-pink-500"></div>
-          <div className="absolute bottom-0 left-0 w-12 h-12 border-b-4 border-l-4 border-pink-500"></div>
-          <div className="absolute bottom-0 right-0 w-12 h-12 border-b-4 border-r-4 border-pink-500"></div>
+           <div className="fixed top-0 left-0 w-12 h-12 border-t-4 border-l-4 border-pink-500"></div>
+           <div className="fixed top-0 right-0 w-12 h-12 border-t-4 border-r-4 border-pink-500"></div>
+           <div className="fixed bottom-0 left-0 z-100 w-12 h-12 border-b-4 border-l-4 border-pink-500"></div>
+           <div className="fixed bottom-0 right-0 z-100 w-12 h-12 border-b-4 border-r-4 border-pink-500"></div>
           {/* Pages demonstrated by the children */}
           <MultiplayerProvider>
             {children}
